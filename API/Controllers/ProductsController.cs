6360ef7525efdbe7a65167811b742ed72fc52113
Adelin <adelin.chis@student.upt.ro>
2020-04-26 00:32:42 +0300

    created a new controller and Entities folder

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "a list of products";
+        }
+
+        [HttpGet("{id}")]
+
+        public string GetProduct(int id)
+        {
+            return id.ToString();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-API-Learning-.Net-Core
+To run this project you must install *.Net Core 3.x *
+Run: '''dotnet watch run''' (make sure you run this command inside the API Folder)
